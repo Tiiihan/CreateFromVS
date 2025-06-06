7b1c80fd68f18af2867b7dc34ff1bfb1a66fa5bc
Yuliia <uliatihovska1@gmail.com>
2025-06-06 18:05:23 +0300

    Added new project

---
+++ SomeNewProject/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter integer a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter integer b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a+b}");
